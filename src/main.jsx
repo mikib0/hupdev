e9f62475f446f20ed0a4acc11fde8258ab6f75bb
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2023-07-03 11:04:29 +0100

    react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
+import App from './App.jsx'
+
+ const router = createHashRouter([
+   {
+     path: '/',
+     element: <App />,
+     errorElement: <h1>Error! 💀</h1>,
+     id: 'root',
+     children: [],
+   },
+ ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
