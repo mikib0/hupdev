b12f12e13a0f8be5748be9f8b43ed4e24418d405
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2023-07-03 10:35:19 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{jsx}'],
+  theme: {
+    colors: {
+      orange: '#FF5C00',
+      'orange-light': '#ffcbae',
+      gray: '#151515',
+      'gray-light': '#252525',
+    },
+  },
+};
+
