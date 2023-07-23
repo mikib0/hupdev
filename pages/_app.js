c0d60379f4b45780e7e763ce156ee1d123e6e00f
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2023-07-23 20:46:22 +0100

    fix viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Head from 'next/head';
 import { useEffect } from 'react';
 import { Footer } from '../components';
 
@@ -14,6 +15,12 @@ export default function App({ Component, pageProps }) {
   }, []);
   return (
     <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+        />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
